dcad4def49778f07593a4e30df9d79bdf219429d
sasanka dilum chamuditha <165694341+sasankadilum@users.noreply.github.com>
2026-05-09 19:27:07 +0530

    Save stable state with 50 Kandy dataset and working UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import ItineraryManager from './itinerary-optimization/ItineraryManager'
+
+function App() {
+  return (
+    <div>
+      <ItineraryManager />
+    </div>
+  )
+}
+
+export default App
