4ef114b5377de48cc4cf9df97500b3d5608371ae
Ishan Wijesinghe <wijesinghelageishan@gmail.com>
2026-08-26 00:16:19 +0530

    Hotels & Activities

--- frontend/constants/tourism/tourismColors.js
+++ frontend/constants/tourism/tourismColors.js
@@ -1,11 +1,11 @@
 const tourismColors = {
   primary: "#3E6650",
   primaryLight: "#E7DBBA",
-  background: "#EEF4FF",
-  white: "#FFFFFF",
+  background: "#F1E9D2",
+  white: "#FBF7EC",
   textDark: "#241F18",
   textLight: "#6F6658",
-  border: "#CBD5E1",
+  border: "#D7CAB0",
 };
 
 export default tourismColors;
